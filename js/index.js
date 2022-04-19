ebe90e75eb2d1cdb0eac90ebb21e9ff2434a3ac2
IcePadSTX <arseniy20031985@yandex.ru>
2022-04-19 12:24:16 +0300

    feat accordion, constructor, validation, modal window, sendForm

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import modalAppearance from "./modules/modalOpen.js";
+import modalClose from "./modules/modalClose.js";
+import validation from "./modules/validation";
+import accordion from "./modules/accordion";
+import sendForm from "./modules/sendForm";
+import constructor from "./modules/constructor";
+
+modalAppearance();
+modalClose();
+formValidation();
+accordionsOpening();
+sendingForm();
+constructorAccordion();
---
+++ js/modules/constructor.js
@@ -0,0 +1,24 @@
+const constructorAccordion = () => {
+    const contructorAccordion = document.getElementById('accordion');
+
+    const allConstructorPanels = contructorAccordion.querySelectorAll('.panel');
+
+    contructorAccordion.addEventListener('click', (e) => {
+        if (e.target.classList.contains('construct-btn')){
+            let parentPanel = e.target.closest('.panel');
+            allConstructorPanels.forEach((constructorPanel, index) => {
+                if (constructorPanel == parentPanel){
+                    let currentContent = allConstructorPanels[index].querySelector('.panel-collapse');
+                    currentContent.style.display = 'none';
+                    if ((index + 1) == allConstructorPanels.length){
+                        
+                    }
+                    let nextContent = allConstructorPanels[index + 1].querySelector('.panel-collapse');
+                    nextContent.style.display = 'block';
+                }
+            })
+        }
+    })
+}
+
+export default constructorAccordion;
